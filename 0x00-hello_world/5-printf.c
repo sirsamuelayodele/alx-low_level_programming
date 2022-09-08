3d7feaf6f583977e210e6f34c6a1901cabae39b7
sirsamuelayodele <asatechafrica@gmail.com>
2022-09-08 07:16:34 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+ptrintf("with proper grammar, but the outcome is a piece of art,\n");
+    return (0);
+}
