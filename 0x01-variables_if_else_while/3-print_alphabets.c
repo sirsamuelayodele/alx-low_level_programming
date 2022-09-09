c9cee7d88047f025413e0c4ca62bf2bdbf59b1c6
sirsamuelayodele <asatechafrica@gmail.com>
2022-09-09 09:14:01 -0700

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,17 +7,16 @@
  */
 int main(void)
 {
-        char lower_case;
-        char upper_case;
-
-        for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
-        {
-                putchar(lower_case);
-        }
-        for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
-	{
-                putchar(upper_case);
-	}
-                putchar('\n');
-        return (0);
+char lower_case;
+char upper_case;
+for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+{
+	putchar(lower_case);
+}
+for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
+{
+	putchar(upper_case);
+}
+putchar('\n');
+return (0);
 }
