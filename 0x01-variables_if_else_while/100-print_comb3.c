6eb72242f00cf1167c1e43801f844f1879ecdfb1
sirsamuelayodele <asatechafrica@gmail.com>
2022-09-09 10:09:48 -0700

    the more brains you use the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,22 +1,23 @@
 #include <stdio.h>
 /**
  * main - main function
- * Return: 0
+ * code for printing all possible combination of two digis in ascending order
  */
 int main(void)
 {
 	int i;
 	int j;
 
-	for (i = 0; i < 10; i++)
+	for (i = 0; i < 9; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = i + 1; j < 10; j++)
 		{
-			putchar(i + '0');
-			putchar(j + '0');
-			if (i == 9 && j == 9)
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (i == 8 && j == 8)
 			{
-				break;
+				continue;
 			}
 			putchar(',');
 			putchar(' ');
