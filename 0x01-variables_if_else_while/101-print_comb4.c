161930c7a77d8c19f1342844d5f0ba93e6380e53
sirsamuelayodele <asatechafrica@gmail.com>
2022-09-09 10:26:31 -0700

    Do what you do better and do more of what you do

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,11 +7,11 @@
  */
 int main(void)
 {
-	int d1, d2, d3
+	int d1, d2, d3;
 
-	for (d1 = 0; d1 < 8; d1++)
+	for (d1 = 0; d1 < 9; d1++)
 	{
-		for (d2 = d1 + 1; d2 < 9; d2++)
+		for (d2 = d1 + 1; d2 < 10; d2++)
 		{
 			for (d3 = d2 + 1; d3 < 10; d3++)
 			{
