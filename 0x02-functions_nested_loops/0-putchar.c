aed3b3dc8e16a37679859d7cd820582ef006f917
sirsamuelayodele <asatechafrica@gmail.com>
2022-09-13 19:31:35 -0700

    A program that prints putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,22 @@
-#include "holberton.h"
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-int main(void)
-{
-	char *sh = "Holberton";
+#include "main.h"
 
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
+/**
+*main - Entry point
+*
+*Return: 0 nothing, return void
+*/
+
+int main(void)
+
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 
 	return (0);
