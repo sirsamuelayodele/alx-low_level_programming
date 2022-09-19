763cc838939af812e7025c17b0bae4ed92b900c5
sirsamuelayodele <asatechafrica@gmail.com>
2022-09-19 06:05:16 -0700

    Task 8 is really a stress

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -1,20 +1,22 @@
 #include "holberton.h"
 #include <stdio.h>
+
 /**
- * print_array - check the code for Holberton School students.
- * @a: is the array.
- * @n: is the size.
+ * print_array - display array
+ * @a:integer
+ * @n:integer
+ *Return:void
  */
 void print_array(int *a, int n)
 {
-	int i;
-
-	for (i = 0; i < n; i++)
+int i;
+for (i = 0; i < n; i++)
 	{
-		printf("%d", a[i]);
-
-		if (i < n - 1)
-			printf(", ");
+	printf("%d", a[i]);
+		if (i < (n - 1))
+		{
+		printf(", ");
+		}
 	}
 	printf("\n");
 }
